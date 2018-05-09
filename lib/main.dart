01e3c20b185a6c39bb534f0de001209b2790d4cb
JPML94 <jp@tradecrafted.com>
2018-05-08 22:37:27 -0700

    MessageList implemented

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,34 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("Friendlychat")),
-      body: _buildTextComposer(),
-    );
-  }
+  return new Scaffold(
+    appBar: new AppBar(title: new Text("Friendlychat")),
+    body: new Column(                                        //modified
+      children: <Widget>[                                         //new
+        new Flexible(                                             //new
+          child: new ListView.builder(                            //new 
+            padding: new EdgeInsets.all(8.0),                     //new
+            reverse: true,                                        //new
+            itemBuilder: (_, int index) => _messages[index],      //new
+            itemCount: _messages.length,                          //new
+          ),                                                      //new
+        ),                                                        //new
+        new Divider(height: 1.0),                                 //new
+        new Container(                                            //new
+          decoration: new BoxDecoration(
+            color: Theme.of(context).cardColor),                  //new
+          child: _buildTextComposer(),                       //modified
+        ),                                                        //new
+      ],                                                          //new
+    ),                                                            //new
+  );
+}
+
 
   Widget _buildTextComposer() {
     return new IconTheme(
@@ -60,6 +79,12 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 }
 
