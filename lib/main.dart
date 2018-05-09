e52d39b81b3490fe8d37f7cb5f78d861cf2a4cbb
JPML94 <jp@tradecrafted.com>
2018-05-08 23:01:15 -0700

    MessageList animation implemented

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
 }
 
 const String _name = "JPMora";
+
 class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,35 +21,46 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-  return new Scaffold(
-    appBar: new AppBar(title: new Text("Friendlychat")),
-    body: new Column(                                        //modified
-      children: <Widget>[                                         //new
-        new Flexible(                                             //new
-          child: new ListView.builder(                            //new 
-            padding: new EdgeInsets.all(8.0),                     //new
-            reverse: true,                                        //new
-            itemBuilder: (_, int index) => _messages[index],      //new
-            itemCount: _messages.length,                          //new
-          ),                                                      //new
-        ),                                                        //new
-        new Divider(height: 1.0),                                 //new
-        new Container(                                            //new
-          decoration: new BoxDecoration(
-            color: Theme.of(context).cardColor),                  //new
-          child: _buildTextComposer(),                       //modified
-        ),                                                        //new
-      ],                                                          //new
-    ),                                                            //new
-  );
-}
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Column(
+        //modified
+        children: <Widget>[
+          //new
+          new Flexible(
+            //new
+            child: new ListView.builder(
+              //new
+              padding: new EdgeInsets.all(8.0), //new
+              reverse: true, //new
+              itemBuilder: (_, int index) => _messages[index], //new
+              itemCount: _messages.length, //new
+            ), //new
+          ), //new
+          new Divider(height: 1.0), //new
+          new Container(
+            //new
+            decoration:
+                new BoxDecoration(color: Theme.of(context).cardColor), //new
+            child: _buildTextComposer(), //modified
+          ), //new
+        ], //new
+      ), //new
+    );
 
+    @override
+    void dispose() {
+      for (ChatMessage message in _messages)
+        message.animationController.dispose();
+      super.dispose();
+    }
+  }
 
   Widget _buildTextComposer() {
     return new IconTheme(
@@ -81,39 +93,50 @@ class ChatScreenState extends State<ChatScreen> {
     _textController.clear();
     ChatMessage message = new ChatMessage(
       text: text,
+      animationController: new AnimationController(
+        duration: new Duration(milliseconds: 200),
+        vsync: this,
+      ),
     );
     setState(() {
       _messages.insert(0, message);
     });
+    message.animationController.forward();
   }
 }
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
   final String text;
+  final AnimationController animationController;
+
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
-          ),
-          new Column(
+    return new SizeTransition(
+        sizeFactor: new CurvedAnimation(
+            parent: animationController, curve: Curves.decelerate),
+        axisAlignment: 0.0,
+        child: new Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: new Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
               new Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(text),
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new CircleAvatar(child: new Text(_name[0])),
+              ),
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
